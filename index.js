e7a63c4a438b866e9b58b035e981fda0779db0eb
shababrc <chowdhuryshababrob@gmail.com>
2023-04-11 20:12:44 -0700

    made comments

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//Creating a CLI constant that gets our 
+//proper app requirement
 const CLI = require('./lib/cli.js');
 
+//creating a new command line input object
+//then calling run
 const cli = new CLI();
 
 cli.run();
